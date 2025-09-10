0c3021decb8c45b796696c7a08d8527e1f6fd9a5
Lncvrt <lncvrtreal@gmail.com>
2025-09-09 22:47:08 -0700

    Add profiles button to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,8 @@ public class MenuScript : MonoBehaviour
     public TMP_Text updateText;
     public Button updateButton;
     public GameObject selfBuiltPanel;
+    public Button profileButton;
+    public ProfileMenu profilePrefab;
 
     void Awake()
     {
@@ -27,5 +29,51 @@ public class MenuScript : MonoBehaviour
             });
         }
         if (SensitiveInfo.SERVER_RECEIVE_TRANSFER_KEY.Trim() == string.Empty || SensitiveInfo.SERVER_SEND_TRANSFER_KEY.Trim() == string.Empty) selfBuiltPanel.SetActive(true);
+    }
+
+    void Start()
+    {
+        if (BazookaManager.Instance.GetAccountID() != null)
+        {
+            profileButton.transform.GetChild(0).GetComponent<TMP_Text>().text = BazookaManager.Instance.GetAccountName();
+            profileButton.onClick.AddListener(async () =>
+            {
+                var clone = Instantiate(profilePrefab.gameObject, profilePrefab.gameObject.transform.parent);
+                clone.SetActive(true);
+                var customIconData = BazookaManager.Instance.GetCustomBirdIconData();
+                string customIcon = null;
+                if (customIconData.Selected != null)
+                {
+                    foreach (var icon in customIconData.Data)
+                    {
+                        if (icon.UUID == customIconData.Selected)
+                        {
+                            customIcon = icon.Data;
+                        }
+                    }
+                }
+                var iconColor = BazookaManager.Instance.GetColorSettingIcon();
+                var overlayColor = BazookaManager.Instance.GetColorSettingOverlay();
+                await clone.GetComponent<ProfileMenu>().Init(
+                    BazookaManager.Instance.GetGameStoreTotalNormalBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalPoisonBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalSlowBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalUltraBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalSpeedyBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalCoinBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalRandomBerries().ToString(),
+                    BazookaManager.Instance.GetGameStoreTotalAntiBerries().ToString(),
+                    customIconData.Balance.ToString(),
+                    BazookaManager.Instance.GetAccountName().ToString(),
+                    BazookaManager.Instance.GetAccountID() ?? 0,
+                    BazookaManager.Instance.GetBirdIcon(),
+                    BazookaManager.Instance.GetBirdOverlay(),
+                    customIcon,
+                    new Color((int)iconColor[0] / 255f, (int)iconColor[1] / 255f, (int)iconColor[2] / 255f),
+                    new Color((int)overlayColor[0] / 255f, (int)overlayColor[1] / 255f, (int)overlayColor[2] / 255f)
+                );
+            });
+            profileButton.gameObject.SetActive(true);
+        }
     }
 }
